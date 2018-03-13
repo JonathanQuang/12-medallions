5caafd3dd8e0097d23a534d21656477b8bc7fda7
JonathanQuang <jquang@stuy.edu>
2018-03-13 13:59:06 -0400

    initial commit

---
+++ medal.js
@@ -0,0 +1,12 @@
+var germany = [14,10,7];
+var china = [1,6,2];
+
+var svg = document.getElementById("svg1");
+
+var generateCircles = function(){
+	document.createElementNS("https://www.w3.org/2000/svg","circle");
+
+
+
+
+}
